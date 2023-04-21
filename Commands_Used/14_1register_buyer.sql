8a000c3550ac48dd68d6c77ba2a2aa56f4e38c33
JaiMand <129975166+JaiMand@users.noreply.github.com>
2023-04-21 16:03:38 +0100

    Add files via upload

--- Commands_Used/14_1register_buyer.sql
+++ Commands_Used/14_1register_buyer.sql
@@ -1,2 +1,9 @@
 USE ManagingProperties;
 
+INSERT INTO customers (first_name, surname, email, address, postcode, phone_number)
+SELECT 'John', 'Doe', 'johndoe@example.com', '456 Main St', '8fh 8hi', 07895551234
+WHERE NOT EXISTS (
+  SELECT *
+  FROM customers
+  WHERE first_name = 'John' AND surname = 'Doe'
+);
